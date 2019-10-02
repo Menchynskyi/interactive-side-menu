0a8ff33eb25a0e85f40aa1e0165de2608908793f
Menchynskyi <the.mngs@yahoo.com>
2019-10-02 20:32:34 +0300

    Responsive

--- data/data.js
+++ data/data.js
@@ -1,13 +1,13 @@
 const musicData = [
     {
         title: 'Play Music',
-        price: '$9.99',
+        price: '$9.99 per month',
         description: 'Play Music is a music and podcast streaming service and online music locker operated by, part of its Play line of services. The service was announced on May 10, 2011, and after a six-month, invitation-only beta period, it was publicly launched on November 16, 2011. Users with standard accounts can upload and listen to up to 50,000 songs from their personal libraries at no cost. A paid Play Music subscription entitles users to on-demand streaming of any song in the Play Music catalog, as well as access to YouTube Music Premium. Users in Canada, the United States, Australia, New Zealand, Mexico, Austria, Finland, France, Germany, Ireland, Italy, Norway, Russia, Spain, Sweden, and the United Kingdom also have access to YouTube Premium. Users can purchase additional tracks for their library through the music store section of Play. In addition to offering music streaming for Internet-connected devices, the Play Music mobile apps allow music to be stored and listened to offline.',
         imgPath: 'img/play.png'
     },
     {
         title: 'Apple Music',
-        price: '$9.99',
+        price: '$9.99 per month',
         description: 'Apple Music is a music and video streaming service developed by Apple Inc. Users select music to stream to their device on-demand, or they can listen to existing, curated playlists. The service also includes the Internet radio station Beats 1, which broadcasts live to over 100 countries 24 hours a day. The service was announced on June 8, 2015, and launched on June 30, 2015 in over 100 countries worldwide. New subscribers get a three-month free trial period before the service requires a monthly subscription. Originally strictly a music service, Apple Music began expanding into video in 2016. Executive Jimmy Iovine has stated that the intention for the service is to become a \"cultural platform\", and Apple reportedly wants the service to be a \"one-stop shop for pop culture\". The company is actively investing heavily in the production and purchasing of video content, both in terms of music videos and concert footage that support music releases, as well as web series and feature films.',
         imgPath: 'img/apple.png'
     },
@@ -19,19 +19,19 @@ const musicData = [
     },
     {
         title: 'YouTube Music',
-        price: '$9.99',
+        price: '$9.99 per month',
         description: 'YouTube Music is a music streaming service and mobile app developed by YouTube; it provides a tailored interface for the service oriented towards music streaming, allowing users to browse through songs and music videos on YouTube based on genres, playlists, and recommendations. The service also offers a premium tier, which enables ad-free playback, audio-only background playback, and downloading songs for offline playback. These subscription benefits are also offered to subscribers of Play Music and YouTube Premium.',
         imgPath: 'img/youtube.png'
     },
     {
         title: 'Deezer',
-        price: '$9.99',
+        price: '$9.99 per month',
         description: 'Deezer is a French online music streaming service. It allows users to listen to music content from record labels, including Universal Music Group, Sony Music and Warner Music Group (owned by Deezer\'s parent company Access Industries) on various devices online or offline. Created in Paris, France, Deezer currently has 56 million licensed tracks in its library, with over 30,000 radio channels, 14 million monthly active users, and 7 million paid subscribers as of January 2019. The service is available for Web, Android, iOS, Windows Mobile, BlackBerry OS and Windows, MacOS.',
         imgPath: 'img/deezer.png'
     },
     {
         title: 'Spotify',
-        price: '$9.99',
+        price: '$9.99 per month',
         description: 'Spotify Technology S.A. is a media-services provider founded in 2006 in Sweden and now incorporated in Luxembourg. The company\'s primary business is its audio streaming platform that provides DRM-protected music and podcasts from record labels and media companies. As a freemium service, basic features are free with advertisements or automatic music videos, while additional features, such as improved streaming quality, are offered via paid subscriptions. Launched on 7 October 2008, the Spotify platform provides access to over 50 million tracks.',
         imgPath: 'img/spotify.png'
     }  
